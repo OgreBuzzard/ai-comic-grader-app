37986ea133ff1558faae08a4eb32824732839f6a
OgreBuzzard <32827666+OgreBuzzard@users.noreply.github.com>
2026-06-14 12:03:08 -0700

    Add files via upload

--- make-ios-index.mjs
+++ make-ios-index.mjs
@@ -224,7 +224,7 @@ mustReplace('D7b splash CSS',
     #splash-subtitle {
       position: absolute;
       left: 50%;
-      top: calc(env(safe-area-inset-top, 0px) + 5vh + 7.5vh);
+      top: calc(env(safe-area-inset-top, 0px) + 5vh + 9vh);
       transform: translate(-50%, -120vh);
       text-align: center;
       font-size: 16px;
